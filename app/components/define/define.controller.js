68010c30e1d0087295aa6fb0cfa18989215aabbb
Tom Saleeba <tom.saleeba@gmail.com>
2017-04-18 18:14:44 +0930

    got some basic map drawing working

--- app/components/define/define.controller.js
+++ app/components/define/define.controller.js
@@ -1,12 +1,16 @@
 /* @ngInject */
 class DefineController {
-  constructor ($scope) {
+  constructor ($scope, leafletDrawEvents, leafletData) {
     this.$scope = $scope
+    this.leafletDrawEvents = leafletDrawEvents
+    this.leafletData = leafletData
     this.configureScope()
   }
 
   configureScope () {
+    L.Icon.Default.imagePath = "https://unpkg.com/leaflet@0.7.7/dist/images" // pretty sure minification forces us to do this. It works in any case
     this.$scope.title = 'Define Baseline'
+    var drawnItems = new L.FeatureGroup()
     this.$scope.map = {
       center: {
         lat: -28.071980301779845,
@@ -21,9 +25,65 @@ class DefineController {
             type: 'xyz'
           }
         }
+      },
+      drawOptions: {
+        position: 'bottomright',
+        draw: {
+          polyline: false,
+          polygon: {
+            allowIntersection: false,
+            metric: true,
+            showArea: true,
+            drawError: {
+              color: '#b00b00',
+              timeout: 1000
+            },
+            shapeOptions: {
+              color: 'blue'
+            }
+          },
+          circle: false,
+          marker: true
+        },
+        edit: {
+          featureGroup: drawnItems,
+          edit: true,
+          remove: true
+        }
       }
     }
+
+    this.$scope.drawnItemsCount = () => {
+      return drawnItems.getLayers().length
+    }
+    function noop () { }
+    function created (e, leafletEvent, leafletObject, model, modelName) {
+      drawnItems.addLayer(leafletEvent.layer)
+    }
+
+    var handle = {
+      created: created,
+      edited: noop,
+      deleted: noop,
+      drawstart: noop,
+      drawstop: noop,
+      editstart: noop,
+      editstop: noop,
+      deletestart: noop,
+      deletestop: noop
+    }
+
+    var drawEvents = this.leafletDrawEvents.getAvailableEvents()
+    drawEvents.forEach((eventName) => {
+      this.$scope.$on('leafletDirectiveDraw.' + eventName, (e, payload) => {
+        var leafletEvent = payload.leafletEvent
+        var leafletObject = payload.leafletObject
+        var model = payload.model
+        var modelName = payload.modelName
+        handle[eventName.replace('draw:', '')](e, leafletEvent, leafletObject, model, modelName)
+      })
+    })
   }
 }
 
-export {DefineController}
+export { DefineController }
