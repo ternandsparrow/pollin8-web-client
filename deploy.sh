bedd2559189c81cfc14f7e2ce2d0d8e2cbf16b86
Tom Saleeba <tom.saleeba@gmail.com>
2017-04-19 12:26:37 +0930

    changed the deploy script so it doesn't keep creating new versions

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,8 @@ if [ "$NPM_RC" != "0" ]; then
 fi
 cp app.yaml $OUTPUT_DIR
 cd $OUTPUT_DIR
-gcloud app deploy app.yaml --stop-previous-version --project pollin8-web-client-162107
+gcloud app deploy app.yaml \
+  --version=therecanonlybeone \
+  --project pollin8-web-client-162107
+# using --stop-previous-version for the previous command doesn't work so we just steamroll the same version
 echo "[SUCCESS] Build and deploy done"
