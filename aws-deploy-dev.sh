f5ec98e300c95dfc761279aa59419de44640da0d
Tom Saleeba <tom.saleeba@gmail.com>
2017-05-02 13:21:15 +0930

    added an AWS dev deploy script

---
+++ aws-deploy-dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd `dirname $0`
+S3_TARGET_BUCKET="www.dev.pollin8.aekos.org.au"
+OUTPUT_DIR="target"
+S3_LS=`sh -c "aws s3 ls | grep $S3_TARGET_BUCKET"`
+if [ -z "$S3_LS" ]; then
+  echo "[ERROR] can't find target S3 bucket '$S3_TARGET_BUCKET', do you have the right account enabled?"
+  exit 1
+fi
+npm run build
+NPM_RC=$?
+if [ "$NPM_RC" != "0" ]; then
+  echo "[ERROR] NPM failed"
+  exit $NPM_RC
+fi
+aws s3 sync --delete ./$OUTPUT_DIR/ s3://$S3_TARGET_BUCKET/
