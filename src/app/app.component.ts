e743c6fe597d839cbac91c12e406413d371846f8
Tom Saleeba <tom.saleeba@gmail.com>
2017-09-01 18:08:56 +0930

    feat: add HERE map as the default so we have satellite imagery with place names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -73,9 +73,23 @@ export class AppComponent implements OnInit {
 			maxZoom: 18,
 			attribution: 'Open Street Map'
 		})
-	}
+  }
+  HERE_hybridDay = L.tileLayer('https://{s}.{base}.maps.cit.api.here.com/maptile/2.1/{type}/{mapID}/hybrid.day/{z}/{x}/{y}/{size}/{format}?app_id={app_id}&app_code={app_code}&lg={language}', {
+    attribution: 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
+    subdomains: '1234',
+    mapID: 'newest',
+    app_id: 'zM6cic4akir1Yp2L2eXF',
+    app_code: 'aEbtMxj4GbBX-3pvDgOMkQ',
+    base: 'aerial',
+    maxZoom: 20,
+    type: 'maptile',
+    language: 'eng',
+    format: 'png8',
+    size: '256'
+  })
 	layersControlOptions = { position: 'bottomright' }
 	baseLayers = {
+    'HERE Satellite': this.HERE_hybridDay,
 		'ESRi Satellite': this.LAYER_ESRI.layer,
 		'Open Street Map': this.LAYER_OSM.layer,
 	}
