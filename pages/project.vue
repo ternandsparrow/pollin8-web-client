5ad06834a65d29fb15a9e04d79595ad4c46958aa
Andrew Tokmakoff <tokmakoff@computer.org>
2020-03-19 12:59:28 +1030

    chore: bump deps (nuxt and others)

--- pages/project.vue
+++ pages/project.vue
@@ -12,7 +12,9 @@
         <v-card-text>
           <p>Blurb of the project. Talk on it more. AP - Get from Katja.</p>
         </v-card-text>
-        <vimeo-player ref="player" :video-id="videoID" @ready="onReady" :player-height="height"></vimeo-player>
+        <no-ssr>
+          <vimeo-player ref="player" :video-id="videoID" @ready="onReady" :player-height="height"></vimeo-player>
+        </no-ssr>
       </v-card>
     </v-flex>
   </v-layout>
